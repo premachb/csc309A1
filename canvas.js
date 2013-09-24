c5524d3b94017b1c175ad85d494c0ef4b6b916cb
Brien Premachandiran <brien@user239-60.wireless.utoronto.ca>
2013-09-24 19:02:47 -0400

    You can draw lines on the canvas now, try it out, its a little broken when you go fast tho

--- canvas.js
+++ canvas.js
@@ -1,10 +1,34 @@
 function draw(){
 	var ctx = $('#myCanvas')[0].getContext('2d');
-	ctx.fillRect(25,25,100,100);
-	ctx.clearRect(45,45,60,60);
-	ctx.strokeRect(50,50,50,50);
+	
 }
 
+
+
 $(document).ready(function() {
-	draw();
+	var lineFinishX, lineFinishY, lineStartX, lineStartY;
+
+
+	// User clicks mouse down to denote where to star their shape
+	$('#myCanvas').mousedown(function(e) {
+		lineStartX = e.clientX;
+		lineStartY = e.clientY;
+
+		console.log("This is the start x: " + lineStartX);
+		console.log("This is the start y: " + lineStartY);
+	});
+
+	// User lifts mouse up to denote where to finish their shape
+	$('#myCanvas').mouseup(function(e){
+		var ctx = $('#myCanvas')[0].getContext('2d');
+		lineFinishX = e.clientX;
+		lineFinishY = e.clientY;
+
+		ctx.beginPath();
+		ctx.moveTo(lineStartX, lineStartY);
+		ctx.lineTo(lineFinishX, lineFinishY);
+		ctx.stroke();
+	});
+
+	
 });
