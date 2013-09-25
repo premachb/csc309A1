83c1aeb32c824dc729efc6ab7a0ad1f10c002e41
Zachary Munro-Cape <zach.munrocape@gmail.com>
2013-09-25 16:00:30 -0400

    added action descriptions below canvas
    Descriptions are placeholders, will need to be improved for usability

--- canvas.js
+++ canvas.js
@@ -2,8 +2,9 @@ function draw(){
 	var ctx = $('#myCanvas')[0].getContext('2d');
 	
 }
-function selector(cur_select){
+function selector(cur_select, button_descriptions){
 	console.log("current action is " + cur_select);
+	document.getElementById("current_action").innerHTML=button_descriptions[cur_select];
 }
 
 function draw_line(lineStartX, lineFinishX, lineStartY, lineFinishY){
@@ -15,9 +16,23 @@ function draw_line(lineStartX, lineFinishX, lineStartY, lineFinishY){
 }
 
 $(document).ready(function() {
+
+	var button_descriptions = {
+		"line" : "Line tool: Click on a starting point and drag to ending point",
+		"triangle" : "insert instructions",
+		"square" : "Click on top left corner of your desired square and then drag to bottom right corner",
+		"erase" : "Click on the object you wish to erase. This action cannot be undone",
+		"move" : "Click on an object you would like to move and drag it to its new position",
+		"resize" : "CLick on an object and drag the bottom right corner until your desired demensions are met",
+		"change" : "Click on an object to modify its color, line color, or ???",
+		"copy" : "Click on an object to store it for a paste action",
+		"paste" : "Click within the canvas to add another object that is the same as the one stored by the copy feature",
+		"clear" : "Remove all objects off the canvas. THIS ACTION CANNOT BE UNDONE"
+	};
+
 	var yoffset = document.getElementById('shape_selector').offsetHeight + document.getElementById('shape_action').offsetHeight;
 	var lineFinishX, lineFinishY, lineStartX, lineStartY;
-	var cur_select = null;
+	var cur_select = "none";
 
 	// User clicks mouse down to denote where to star their shape
 	$('#myCanvas').mousedown(function(e) {
@@ -45,6 +60,6 @@ $(document).ready(function() {
 		//ctx.stroke();
 	});
 
-	$("button").click(function(){ cur_select = $(this).attr('id'); selector(cur_select)});
+	$("button").click(function(){ cur_select = $(this).attr('id'); selector(cur_select, button_descriptions)});
 
 });
