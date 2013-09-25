bb7e9de34ed03cc2ddb4c3e39e60b6656229596b
Zachary Munro-Cape <zach.munrocape@gmail.com>
2013-09-25 15:46:39 -0400

    made function for line drawing and some logic to call it only when line button selected

--- canvas.js
+++ canvas.js
@@ -2,13 +2,22 @@ function draw(){
 	var ctx = $('#myCanvas')[0].getContext('2d');
 	
 }
+function selector(cur_select){
+	console.log("current action is " + cur_select);
+}
 
-
+function draw_line(lineStartX, lineFinishX, lineStartY, lineFinishY){
+	    var ctx = $('#myCanvas')[0].getContext('2d');
+		ctx.beginPath();
+		ctx.moveTo(lineStartX, lineStartY);
+		ctx.lineTo(lineFinishX, lineFinishY);
+		ctx.stroke();
+}
 
 $(document).ready(function() {
 	var yoffset = document.getElementById('shape_selector').offsetHeight + document.getElementById('shape_action').offsetHeight;
 	var lineFinishX, lineFinishY, lineStartX, lineStartY;
-
+	var cur_select = null;
 
 	// User clicks mouse down to denote where to star their shape
 	$('#myCanvas').mousedown(function(e) {
@@ -21,15 +30,21 @@ $(document).ready(function() {
 
 	// User lifts mouse up to denote where to finish their shape
 	$('#myCanvas').mouseup(function(e){
-		var ctx = $('#myCanvas')[0].getContext('2d');
+		//var ctx = $('#myCanvas')[0].getContext('2d');
 		lineFinishX = e.clientX;
 		lineFinishY = e.clientY - yoffset;
-
-		ctx.beginPath();
-		ctx.moveTo(lineStartX, lineStartY);
-		ctx.lineTo(lineFinishX, lineFinishY);
-		ctx.stroke();
+		if(cur_select == 'line'){
+			draw_line(lineStartX, lineFinishX, lineStartY, lineFinishY);
+		}
+		else {
+			console.log("not line action");
+		}
+		//ctx.beginPath();
+		//ctx.moveTo(lineStartX, lineStartY);
+		//ctx.lineTo(lineFinishX, lineFinishY);
+		//ctx.stroke();
 	});
 
-	
+	$("button").click(function(){ cur_select = $(this).attr('id'); selector(cur_select)});
+
 });
