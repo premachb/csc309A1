543606e547679a5b7f2715f363423ba6af41c4f7
Zachary Munro-Cape <zach.munrocape@gmail.com>
2013-10-03 15:42:56 -0400

    Fixed draw square
    started baking in OO concepts

--- canvas.js
+++ canvas.js
@@ -2,7 +2,7 @@ function selector(cur_select, button_descriptions){
 	console.log("current action is " + cur_select);
 	document.getElementById("current_action").innerHTML=button_descriptions[cur_select];
 	if (cur_select == "clear"){
-		erase_canvas();
+		erase_canvas(false);
 	}
 }
 
@@ -24,15 +24,57 @@ function draw_square(startX, startY, finishX, finishY){
 	var ctx = $('#myCanvas')[0].getContext('2d');
 	var height = Math.abs(finishY - startY);
 	var width = Math.abs(finishX - startX);
-
-	ctx.strokeRect(startX, startY, width, height);
+	if(startX < finishX){
+		if(startY < finishY){
+			ctx.strokeRect(startX, startY, width, height);
+		}else{
+			ctx.strokeRect(startX, finishY, width, height);
+		}
+		//ctx.strokeRect(startX, startY, width, height);
+	} else{
+		if(startY < finishY){
+			ctx.strokeRect(finishX, startY, width, height);
+		} else{
+			ctx.strokeRect(finishX, finishY, width, height);
+		}
+		
+	}
+	//ctx.strokeRect(startX, startY, width, height);
 
 }
+function Line(lineStartX, lineFinishX, lineStartY, lineFinishY){
+	//var ctx = $('#myCanvas')[0].getContext('2d');
+	console.log(lineStartX, lineFinishX, lineStartY, lineFinishY);
+	this.lineStartX = lineStartX;
+	this.lineStartY = lineStartY;
+	this.lineFinishX = lineFinishX;
+	this.lineFinishY = lineFinishY;
+	this.color;
+	this.fill="#FF0000";
+	this.outline_width;
+	this.draw=function(ctx){
+		ctx.beginPath();
+		ctx.moveTo(this.lineStartX, this.lineStartY);
+		ctx.lineTo(this.lineFinishX, this.lineFinishY);
+		ctx.stroke();
+	}
+}
+function draw_objects(ctx, shapeArray){
+	var arrLength = shapeArray.length;
+	for(var index = 0; index < arrLength; index++){
+		shapeArray[index].draw(ctx);
+	}
+}
 
-function erase_canvas(){
-	if (confirm ('Are you sure you want to erase all objects on the canvas?')){
+function erase_canvas(internal){
+	if(internal){
 		$('#myCanvas')[0].width = $('#myCanvas')[0].width;
-		$('#current_action').text('Erased Canvas!');
+	}
+	else{
+		if (confirm ('Are you sure you want to erase all objects on the canvas?')){
+			$('#myCanvas')[0].width = $('#myCanvas')[0].width;
+			$('#current_action').text('Erased Canvas!');
+		}
 	}
 }
 
@@ -57,9 +99,10 @@ $(document).ready(function() {
 	var lineFinishX, lineFinishY, lineStartX, lineStartY;
 	var cur_select = "none";
 	var shapes = []; // keep track of the objects currently on the canvas. 
-
+	var mDown = false;
 	// User clicks mouse down to denote where to star their shape
 	$('#myCanvas').mousedown(function(e) {
+
 		if(cur_select == 'line'){
 			lineStartX = e.clientX;
 			lineStartY = e.clientY - yoffset;
@@ -71,6 +114,17 @@ $(document).ready(function() {
 		}
 	});
 
+	//this is where the drawing occurs
+	$(document).mousemove(function(event){
+		if(mDown=true){
+			// object is being drawn
+			// update the screen
+			//draw_objects();
+			//drawTemp();
+		}
+		// used for debugging purposes only - remove this line (eventually)
+		$('#current_action').text(event.pageX + " " + event.pageY);
+  	});
 	// User lifts mouse up to denote where to finish their shape
 	$('#myCanvas').mouseup(function(e){
 		//var ctx = $('#myCanvas')[0].getContext('2d');
@@ -78,6 +132,7 @@ $(document).ready(function() {
 		lineFinishY = e.clientY - yoffset;
 		if(cur_select == 'line'){
 			draw_line(lineStartX, lineFinishX, lineStartY, lineFinishY);
+			var tempLine = new Line(lineStartX, lineFinishX, lineStartY, lineFinishY);
 		}
 		else if(cur_select == 'square'){
 			draw_square(lineStartX, lineStartY, lineFinishX, lineFinishY);
@@ -86,6 +141,8 @@ $(document).ready(function() {
 			console.log("not line action");
 		}
 	});
+
+
 
 	$("button").click(function(){ cur_select = $(this).attr('id'); selector(cur_select, button_descriptions)});
 
